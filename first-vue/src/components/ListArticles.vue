9bb73a6845f2b2a6620b953168419c02540f2fe8
belovada <belovada20@gmail.com>
2026-06-03 23:15:55 +0700

    pagination added

--- first-vue/src/components/ListArticles.vue
+++ first-vue/src/components/ListArticles.vue
@@ -10,6 +10,12 @@
           :description="item.body"
           class="list-articles__card"
         />
+        <ThePaginator
+        :page="page"
+        :totalPages="totalPages"
+        class="list-articles__paginator"
+        @setPage="setPage"
+        />
       </div>
     </div>
   </main>
@@ -17,23 +23,40 @@
 
 <script>
   import CardArticle from "@/components/card/CardArticle.vue";
+  import ThePaginator from "@/components/ThePaginator.vue";
 
   export default {
     components: {
       CardArticle,
+      ThePaginator,
     },
     data() {
       return {
         newsList: [],
+        limit: 10,
+        page: 1,
+        totalCount: 1,
+        totalPages: 1,
       };
     },
     methods: {
-      getNewsList() {
-        this.$axios("https://dummyjson.com/posts").then((response) => {
+      getNewsList(page = 1) {
+        this.$axios("https://dummyjson.com/posts", {
+          params: {
+            limit: this.limit,
+            skip: this.limit * (page - 1),
+          },
+        }).then((response) => {
           if (response?.data?.posts) {
             this.newsList = response.data.posts;
+            this.page = page;
+            this.totalCount = response.data.total;
+            this.totalPages = Math.ceil(this.totalCount / this.limit);
           }
         });
+      },
+      setPage(page) {
+        this.getNewsList(page);
       },
     },
     created() {
